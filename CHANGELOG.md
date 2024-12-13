29e73de0daf35d3c89e026f4bc51cfd4c1ff2d1c
gllm-dev <guillem@openfort.xyz>
2024-12-13 12:19:35 +0100

    feat: metrics server

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.0.5] - 2024-13-03
+### Added
+- HTTP Server for metrics
+
 ## [0.0.4] - 2024-13-03
 ### Fixed
 - Use info.FullMethod for GRPC Unary Middleware
--- exposer.go
+++ exposer.go
@@ -1,6 +1,8 @@
 package metrics
 
 import (
+	"context"
+	"fmt"
 	"net/http"
 
 	"github.com/prometheus/client_golang/prometheus/promhttp"
@@ -9,4 +11,29 @@ import (
 // ExposeHTTP returns a http.Handler that exposes the metrics
 func ExposeHTTP() http.Handler {
 	return promhttp.Handler()
+}
+
+// Server is a metrics server
+type Server struct {
+	server *http.Server
+}
+
+// NewServer creates a new metrics server
+func NewServer(port int) *Server {
+	return &Server{
+		server: &http.Server{
+			Addr: fmt.Sprintf(":%d", port),
+		},
+	}
+}
+
+// Start starts the metrics server
+func (s *Server) Start(ctx context.Context) error {
+	http.Handle("/metrics", ExposeHTTP())
+	return s.server.ListenAndServe()
+}
+
+// Stop stops the metrics server
+func (s *Server) Stop(ctx context.Context) error {
+	return s.server.Shutdown(ctx)
 }
